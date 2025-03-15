e08d08a75daf4dbdae3283d0d05c41eb6ebbdaaf
hwtrs <hwatrs77@gmail.com>
2025-03-15 14:34:41 -0400

    sql set up

--- scripts/server.js
+++ scripts/server.js
@@ -1,5 +1,20 @@
-const express = require("express");
-const mysql = require("mysql2");
-const bcrypt = require("bcryptjs");
-const cors = require("cors");
+import mysql from 'mysql2'
 
+import dotenv from 'dotenv'
+dotenv.config()
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: process.env.MYSQL_PASSWORD,
+    database: 'notes_app'
+}).promise()
+
+
+async function getNodes() {
+    const [rows]  = await pool.query("SELECT * FROM notes");
+    console.log(rows)
+}
+
+const notes = await getNodes()
+console.log(notes)
