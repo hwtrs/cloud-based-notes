1a7a02eec38ad6d991fab566151467a07ad566e7
hwtrs <hwatrs77@gmail.com>
2025-03-28 21:32:37 -0400

    server should work now

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import app from "../server.js";
 import { createServer } from "@vercel/node";
 
-export default createServer(app);
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/test', (req, res) => {
+  res.json({ message: "Server is working!" });
+});
+
+export default app;
