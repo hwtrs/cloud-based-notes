01d7619b15cd4e092afbf8e6aa3771dd8bae6851
hwtrs <hwatrs77@gmail.com>
2025-03-28 21:59:19 -0400

    forgot the .js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
-import app from "../scripts/login_handler";
+import app from "../scripts/login_handler.js";
 
-export default (req, res) => {
-    app(req, res);
-};
+export default function handler(req, res) {
+    return app(req, res);
+}
